cc61715dc2ee365ae6f6cba3db209d634fac3e34
Abeer Alhuzali <aalhuz2@uic.edu>
2018-08-01 15:44:32 -0500

    Update run.pl

--- run.pl
+++ run.pl
@@ -7,6 +7,7 @@ printf("mypath = $mypath\n");
 $mem_min=32;
 $mem_max=10240;   
 
+# edit the path of neo4j..../lib to your neo4j installation folder
 $classpath="${mypath}/../../neo4j-community-2.1.5/lib/*:${mypath}/lib/*:${classpath_separator}${mypath}/bin";
 
 system("java -XX:+HeapDumpOnOutOfMemoryError -Dnavex.home=\"${mypath}\" -classpath \"${classpath}\" navex.Main  @ARGV");
